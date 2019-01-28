5fecb91ca012c24a4129b20becc817fa75c38098
x7c1 <jmtaro+x7c1@gmail.com>
2019-01-29 00:18:25 +0900

    draft

---
+++ skyblue-domain/src/test/scala/x7c1/skyblue/domain/GremlinSampleRunnerTest.scala
@@ -0,0 +1,9 @@
+package x7c1.skyblue.domain
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class GremlinSampleRunnerTest extends FlatSpec with Matchers {
+  "runner" can "connect to gremlin server" in {
+    true shouldBe true
+  }
+}
