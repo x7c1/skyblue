703916645361a6d171d7394e28e6dd8f8e4383cc
x7c1 <jmtaro+x7c1@gmail.com>
2019-01-31 00:28:09 +0900

    add scala-java8-compat

--- project/SkyblueSettings.scala
+++ project/SkyblueSettings.scala
@@ -5,7 +5,7 @@ import sbt.Def.SettingList
 object SkyblueSettings {
   lazy val commons = new SettingList(Seq(
     // https://mvnrepository.com/artifact/org.scala-lang/scala-library
-    scalaVersion := "2.12.6",
+    scalaVersion := "2.12.8",
     scalacOptions ++= Seq(
       "-deprecation",
       "-feature",
@@ -16,6 +16,8 @@ object SkyblueSettings {
     libraryDependencies ++= Seq(
       // https://mvnrepository.com/artifact/org.scalatest/scalatest
       "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+      // https://maven-repository.com/artifact/org.scala-lang.modules/scala-java8-compat_2.12
+      "org.scala-lang.modules" % "scala-java8-compat_2.12" % "0.9.0",
     ),
   ))
 }
